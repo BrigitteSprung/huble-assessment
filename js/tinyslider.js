6a9f3c55ed9f15e647004ea54539925501df8aea
BrigitteSprung <brigittesprung@gmail.com>
2021-06-08 00:30:51 +0200

    Moved folders around

---
+++ js/tinyslider.js
@@ -0,0 +1,28 @@
+var slider = tns({
+    container: '.my-slider',
+    items: 1,
+    controls: false,
+    nav: false,
+    arrowKeys: true,
+    responsive: {
+        600: {
+            items: 2,
+            gutter: 20,
+        },
+        640: {
+          edgePadding: 20,
+          gutter: 20,
+          items: 3
+        },
+        900: {
+          items: 5
+        },
+        1600: {
+            items: 6
+        }
+    },
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayButtonOutput: false,
+    mouseDrag: true
+  });
